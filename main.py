7a9829e5403bd3c968ab76a07e6978965dd5e6db
Aditi Ravi <69166723+AditiRavi@users.noreply.github.com>
2021-07-09 04:33:31 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,101 @@
+import cv2
+import numpy as np
+
+frameWidth = 640
+frameHeight = 480
+
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+
+
+def empty(a):
+    pass
+
+
+cv2.namedWindow("Parameters")
+cv2.resizeWindow("Parameters", 640, 240)
+cv2.createTrackbar("Threshold1", "Parameters", 25, 255, empty)
+cv2.createTrackbar("Threshold2", "Parameters", 51, 255, empty)
+cv2.createTrackbar("Area", "Parameters", 1250, 30000, empty)
+
+
+def stackImage(imgArray, scale, lables=[]):
+    sizeW = imgArray[0][0].shape[1]
+    sizeH = imgArray[0][0].shape[0]
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range(0, rows):
+            for y in range(0, cols):
+                imgArray[x][y] = cv2.resize(imgArray[x][y], (sizeW, sizeH), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank] * rows
+        hor_con = [imageBlank] * rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+            hor_con[x] = np.concatenate(imgArray[x])
+        ver = np.vstack(hor)
+        ver_con = np.concatenate(hor)
+    else:
+        for x in range(0, rows):
+            imgArray[x] = cv2.resize(imgArray[x], (sizeW, sizeH), None, scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor = np.hstack(imgArray)
+        hor_con = np.concatenate(imgArray)
+        ver = hor
+    if len(lables) != 0:
+        eachImgWidth = int(ver.shape[1] / cols)
+        eachImgHeight = int(ver.shape[0] / rows)
+        print(eachImgHeight)
+        for d in range(0, rows):
+            for c in range(0, cols):
+                cv2.rectangle(ver, (c * eachImgWidth, eachImgHeight * d),
+                              (c * eachImgWidth + len(lables[d][c]) * 13 + 27, 30 + eachImgHeight * d), (255, 255, 255),
+                              cv2.FILLED)
+                cv2.putText(ver, lables[d][c], (eachImgWidth * c + 10, eachImgHeight * d + 20),
+                            cv2.FONT_HERSHEY_COMPLEX, 0.7, (255, 0, 255), 2)
+    return ver
+
+
+def getcontours(img, imgContour):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        areaMin = cv2.getTrackbarPos("Area", "Parameters")
+        if area > areaMin:
+            cv2.drawContours(imgContour, cnt, -1, (255, 0, 255), 7)
+            peri = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
+            x, y, w, h = cv2.boundingRect(approx)
+            cv2.rectangle(imgContour, (x, y), (x + w, y + h), (0, 255, 0), 5)
+            cv2.putText(imgContour, "Points: " + str(len(approx)), (x + w + 20, y + 20), cv2.FONT_HERSHEY_COMPLEX, .7,
+                        (0, 255, 0), 2)
+            cv2.putText(imgContour, "Area: " + str(area), (x + w + 20, y + 45), cv2.FONT_HERSHEY_COMPLEX, .7,
+                        (0, 255, 0), 2)
+
+
+while True:
+    success, img = cap.read()
+    imgContour = img.copy()
+    imgBlur = cv2.GaussianBlur(img, (7, 7), 1)
+    imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
+
+    threshold1 = cv2.getTrackbarPos("Threshold1", "Parameters")
+    threshold2 = cv2.getTrackbarPos("Threshold2", "Parameters")
+    imgCanny = cv2.Canny(imgGray, threshold1, threshold2)
+    kernel = np.ones((5, 5))
+    imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
+    _img = cv2.resize(img, (300, 300))
+    getcontours(imgDil, imgContour)
+    imgStack = stackImage(([_img, imgGray, imgCanny],
+                           [imgDil, imgContour, imgContour]), 0.6)
+
+    cv2.imshow("Result", imgStack)
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
